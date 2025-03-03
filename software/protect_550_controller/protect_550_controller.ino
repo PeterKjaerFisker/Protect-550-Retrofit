b28a55b68c08d88bbbc73484b3756195e644970a
Peter_Fisker <pet5631@gmail.com>
2025-03-03 21:23:20 +0100

    moved controller script to subfolder so it can open in Arduino IDE. also updated script with pins for LEDs and added a timer-based interrupt for turning off pump. The timer assumes a zero crossing frequency of 0.25 Hz, so it is easier to debug with print statements.

--- software/protect_550_controller/protect_550_controller.ino
+++ software/protect_550_controller/protect_550_controller.ino
@@ -14,8 +14,23 @@
 
 #include <Arduino.h>
 #include <FunctionalInterrupt.h>
+#include "esp_system.h"
 
-#define ZeroCrossPin 20
+#define ZeroCrossPin 23
+#define RedLedPin 4
+#define YellowLedPin 5
+#define GreenLedPin 8
+#define HeatingElementPin 6
+#define UserButtonPin 3
+
+hw_timer_t *timer = NULL;
+
+void ARDUINO_ISR_ATTR pump_stop_isr()
+{
+  Serial.println("Stopping the pump");
+  // Turn off the pump
+  digitalWrite(GreenLedPin, LOW); 
+}
 
 class SmokeMachine
 {
@@ -29,6 +44,7 @@ public:
   {
     attachInterrupt(PIN, std::bind(&SmokeMachine::isr, this), RISING);
     Serial.printf("Started SmokeMachine interrupt on pin %d\n", PIN);
+  
   }
 
   ~SmokeMachine()
@@ -39,15 +55,18 @@ public:
   bool check_temperature()
   {
     // Read temperature from sensor
-    temperature = random(0, 500);
+    // temperature = random(0, 500);
+    temperature = analogRead(HeatingElementPin);
 
     // Check if temperature is above threshold
     if (temperature > threshold)
     {
+      // Serial.printf("Temperature is above threshold: %u\n", temperature);
       return true;
     }
     else
     {
+      // Serial.printf("Temperature is below threshold: %u\n", temperature);
       return false;
     }
   }
@@ -55,26 +74,35 @@ public:
   void start_heater()
   {
     // Turn on heater
-    // digitalWrite(11, HIGH);
+    digitalWrite(RedLedPin, HIGH);
     heater_on = true;
   }
 
   void stop_heater()
   {
     // Turn off heater
-    // digitalWrite(11, LOW);
+    digitalWrite(RedLedPin, LOW);
     heater_on = false;
+  }
+
+  void start_pump()
+  {
+    // Turn on the pump
+    digitalWrite(GreenLedPin, HIGH);
   }
 
   void ARDUINO_ISR_ATTR isr()
   {
+    pressed = true;
+    numberKeyPresses++;
+    // Serial.printf("isr activated");
     // Check if temperature is above threshold so machine is ready
     bool temp_ready = check_temperature();
 
     if (temp_ready)
     {
       // Turn on a status LED to indicate the machine is ready
-      // digitalWrite(13, HIGH);
+      digitalWrite(YellowLedPin, HIGH);
       // turn off heater if is on
       if (heater_on)
       {
@@ -84,7 +112,7 @@ public:
     else
     {
       // Turn off the status LED to indicate the machine is not ready
-      // digitalWrite(13, LOW);
+      digitalWrite(YellowLedPin, LOW);
 
       // Turn on heater if is off
       if (!heater_on)
@@ -93,20 +121,25 @@ public:
       }
     }
 
+    // Serial.printf("user button state is: %u\n", digitalRead(UserButtonPin));
     // Check if the button is pressed
-    if (digitalRead(PIN) == LOW)
+    if (digitalRead(UserButtonPin) == LOW)
     {
       // If the button is pressed and the machine is ready, turn on the machine
+      Serial.println("User pressing button.");
+
       if (temp_ready)
       {
         // Turn on the pump
-        // start_pump();
+        start_pump();
 
         // start timer based ISR to turn off the pump later
-        //
+        timerRestart(timer);
+        timerAlarm(timer, 1500, false, 0);
       }
     }
   }
+
 
   void checkPressed()
   {
@@ -121,8 +154,8 @@ private:
   const uint8_t PIN;
   volatile uint32_t numberKeyPresses;
   volatile bool pressed;
-  uint8_t temperature = 0;
-  uint8_t threshold = 250;
+  uint16_t temperature = 0;
+  uint16_t threshold = 500;
   bool heater_on = false;
 };
 
@@ -131,13 +164,25 @@ SmokeMachine machine(ZeroCrossPin);
 void setup()
 {
   Serial.begin(115200);
+  pinMode(RedLedPin, OUTPUT);
+  pinMode(YellowLedPin, OUTPUT);
+  pinMode(GreenLedPin, OUTPUT);
 
+  // pinMode(HeatingElementPin, INPUT_PULLDOWN);
+  pinMode(UserButtonPin, INPUT_PULLUP);
+  
   Serial.println("Starting Functional Interrupt example.");
   machine.begin();
+
+  timer = timerBegin(1000);
+  timerAttachInterrupt(timer, &pump_stop_isr);
+  // timerAlarm(timer, 10, false, 0);
+
+  
   Serial.println("Setup done.");
 }
 
 void loop()
 {
-  machine.checkPressed();
+  // machine.checkPressed();
 }
