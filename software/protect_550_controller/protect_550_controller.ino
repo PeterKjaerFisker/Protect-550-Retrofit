763274c77d8b984484a96f0e418e2a50eb53a3d8
Peter_Fisker <pet5631@gmail.com>
2025-03-31 20:51:00 +0200

    Add potmeter control of pump duration - Added an additional pin for zero crossing interrupt, as we can't have a falling and rising interrupt on the same pin. - moved some of the pins around to clean up the board.

--- software/protect_550_controller/protect_550_controller.ino
+++ software/protect_550_controller/protect_550_controller.ino
@@ -16,10 +16,12 @@
 #include <FunctionalInterrupt.h>
 #include "esp_system.h"
 
-#define ZeroCrossPin 23
-#define RedLedPin 4
-#define YellowLedPin 5
-#define GreenLedPin 8
+#define PotentiometerPin 4
+#define ZeroCrossPin1 22
+#define ZeroCrossPin2 23
+#define RedLedPin 21
+#define YellowLedPin 20
+#define GreenLedPin 19
 #define HeatingElementPin 6
 #define UserButtonPin 3
 
@@ -29,35 +31,38 @@ void ARDUINO_ISR_ATTR pump_start_isr()
 {
   Serial.println("Starting the pump");
   // Turn on the pump
-  digitalWrite(GreenLedPin, HIGH); 
+  digitalWrite(GreenLedPin, HIGH);
 }
 
 class SmokeMachine
 {
 public:
-  SmokeMachine(uint8_t reqPin) : PIN(reqPin)
+  SmokeMachine(uint8_t risingPin, uint8_t fallingPin)
+  : risingPin(risingPin)
+  , fallingPin(fallingPin)
   {
-    pinMode(PIN, INPUT_PULLUP);
+    pinMode(risingPin, INPUT_PULLUP);
+    pinMode(fallingPin, INPUT_PULLUP);
   };
 
   void begin()
   {
-    attachInterrupt(PIN, std::bind(&SmokeMachine::main_isr, this), RISING);
-    attachInterrupt(PIN, std::bind(&SmokeMachine::pump_stop_isr, this), FALLING);
-    Serial.printf("Started SmokeMachine interrupt on pin %d\n", PIN);
+    attachInterrupt(risingPin, std::bind(&SmokeMachine::main_isr, this), RISING);
+    attachInterrupt(fallingPin, std::bind(&SmokeMachine::pump_stop_isr, this), FALLING);
+    Serial.printf("Started SmokeMachine interrupt on pins %d and %d\n", risingPin, fallingPin);
   }
 
   ~SmokeMachine()
   {
-    detachInterrupt(PIN);
+    detachInterrupt(risingPin);
+    detachInterrupt(fallingPin);
   }
 
-  
   bool check_temperature()
   {
     // Read temperature from sensor
     temperature = analogRead(HeatingElementPin);
-    
+
     // Check if temperature is above threshold
     if (temperature > threshold)
     {
@@ -70,26 +75,44 @@ public:
       return false;
     }
   }
-  
+
   void start_heater()
   {
     // Turn on heater
     digitalWrite(RedLedPin, HIGH);
     heater_on = true;
   }
-  
+
   void stop_heater()
   {
     // Turn off heater
     digitalWrite(RedLedPin, LOW);
     heater_on = false;
   }
-  
+
+  int get_pump_delay()
+  {
+    int delayValue = 0;
+    // Read the potentiometer value
+    potValue = analogRead(PotentiometerPin);
+
+    // The max potentiometer value depends on the voltage read when the potmeter 
+    // is turned to max. 
+    int potMax = 3665;
+
+    // Map the potentiometer value to one of 4 values.
+    delayValue = map(potValue, 0, potMax, 0, 3);
+
+    // set delay between 500 and 2000 milliseconds
+    // Serial.printf("Potmeter reads: %u -> delay: %u\n", potValue, (delayValue+1)*500);
+    return (delayValue+1)*500;
+
+
   void ARDUINO_ISR_ATTR pump_stop_isr()
   {
     Serial.println("Stopping the pump");
     // Turn off the pump
-    digitalWrite(GreenLedPin, LOW); 
+    digitalWrite(GreenLedPin, LOW);
   }
 
   void ARDUINO_ISR_ATTR main_isr()
@@ -132,33 +155,35 @@ public:
 
       if (temp_ready)
       {
+        int pump_delay = get_pump_delay();
         // start timer based ISR to turn on the pump later
         timerRestart(timer);
-        timerAlarm(timer, 1500, false, 0);
+        timerAlarm(timer, pump_delay, false, 0);
       }
     }
   }
-
 
   void checkPressed()
   {
     if (pressed)
     {
-      Serial.printf("SmokeMachine on pin %u has been pressed %lu times\n", PIN, numberKeyPresses);
+      Serial.printf("SmokeMachine with risingPin on pin %u has been pressed %lu times\n", risingPin, numberKeyPresses);
       pressed = false;
     }
   }
 
 private:
-  const uint8_t PIN;
+  const uint8_t risingPin;
+  const uint8_t fallingPin;
   volatile uint32_t numberKeyPresses;
   volatile bool pressed;
   uint16_t temperature = 0;
+  uint16_t potValue = 0;
   uint16_t threshold = 500;
   bool heater_on = false;
 };
 
-SmokeMachine machine(ZeroCrossPin);
+SmokeMachine machine(ZeroCrossPin1, ZeroCrossPin2);
 
 void setup()
 {
@@ -167,17 +192,14 @@ void setup()
   pinMode(YellowLedPin, OUTPUT);
   pinMode(GreenLedPin, OUTPUT);
 
-  // pinMode(HeatingElementPin, INPUT_PULLDOWN);
   pinMode(UserButtonPin, INPUT_PULLUP);
-  
+
   Serial.println("Starting Functional Interrupt example.");
   machine.begin();
 
   timer = timerBegin(1000);
   timerAttachInterrupt(timer, &pump_start_isr);
-  // timerAlarm(timer, 10, false, 0);
 
-  
   Serial.println("Setup done.");
 }
 
